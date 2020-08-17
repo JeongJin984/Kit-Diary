04a5a65a2902e65c1d185e1d28e94a94e6b9f1c6
outerskyb <outerskyb@gmail.com>
2020-08-17 22:37:14 +0900

    impl join(signup)

--- back/routes/user.js
+++ back/routes/user.js
@@ -6,5 +6,8 @@ router.post('/login', passport.authenticate('login', {
     successRedirect: '/',
     failureRedirect: '/login'
 }))
-
+router.post('/join', passport.authenticate('join',{
+    successRedirect: '/',
+    failureRedirect: '/join'
+}))
 module.exports = router
